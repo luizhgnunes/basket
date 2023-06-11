ab12bf00983d8ea43f1ac5e6842e2092567012a9
Luiz Nunes <lnunes@lohika.com>
2023-06-11 23:24:08 +0100

    Added OrderController and ProductController

---
+++ Basket.Api/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Basket.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public OrderController()
+        {
+        }
+
+        // POST api/order
+        [HttpPost]
+        public async Task<ActionResult<OrderResponse>> PostCreateOrder(Guid basketId)
+        {
+            return null;
+        }
+    }
+}
---
+++ Basket.Api/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Basket.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+        }
+
+        // GET: api/product/top-ranked
+        [HttpGet("top-ranked")]
+        public async Task<ActionResult<IEnumerable<ProductResponse>>> GetTopRankedProducts()
+        {
+            return null;
+        }
+
+        // GET api/product/catalog/100/1
+        [HttpGet("catalog/{pageSize}/{pageNumber}")]
+        public async Task<ActionResult<IEnumerable<ProductResponse>>> GetProductCatalog(int pageSize = 100, int pageNumber = 1)
+        {
+            return null;
+        }
+
+        // GET api/product/cheapest
+        [HttpGet("cheapest")]
+        public async Task<ActionResult<IEnumerable<ProductResponse>>> GetCheapestProducts()
+        {
+            return null;
+        }
+
+    }
+}
