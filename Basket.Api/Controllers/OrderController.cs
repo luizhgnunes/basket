565661a04ecd280234b7eb6ff896832d679c4ebd
Luiz Nunes <lnunes@lohika.com>
2023-06-12 00:56:59 +0100

    Added create order functinalities.

--- Basket.Api/Controllers/OrderController.cs
+++ Basket.Api/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Basket.Common.Models;
+﻿using Basket.Common.Interfaces.BusinessLogic;
+using Basket.Common.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.Api.Controllers
@@ -7,15 +8,19 @@ namespace Basket.Api.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
-        public OrderController()
+        private readonly IOrderBusinessLogic _orderBusinessLogic;
+
+        public OrderController(IOrderBusinessLogic orderBusinessLogic)
         {
+            _orderBusinessLogic = orderBusinessLogic;
         }
 
         // POST api/order
         [HttpPost]
         public async Task<ActionResult<OrderResponse>> PostCreateOrder(Guid basketId)
         {
-            return null;
+            var result = await _orderBusinessLogic.CreateOrderAsync(basketId);
+            return Ok(result);
         }
     }
 }
--- Basket.BusinessLogic/OrderBusinessLogic.cs
+++ Basket.BusinessLogic/OrderBusinessLogic.cs
@@ -1,18 +1,31 @@
-﻿using Basket.Common.Interfaces.BusinessLogic;
+﻿using Basket.Common.CodeChallengeApi;
+using Basket.Common.Interfaces.BusinessLogic;
 using Basket.Common.Models;
+using RestSharp;
 
 namespace Basket.BusinessLogic
 {
     public class OrderBusinessLogic : IOrderBusinessLogic
     {
+        private readonly CodeChallengeApiClient _client;
+        private readonly ILoginBusinessLogic _loginBusinessLogic;
+        private readonly IBasketBusinessLogic _basketBusinessLogic;
 
-        public OrderBusinessLogic()
+        public OrderBusinessLogic(CodeChallengeApiClient client, ILoginBusinessLogic loginBusinessLogic, IBasketBusinessLogic basketBusinessLogic)
         {
+            _client = client;
+            _loginBusinessLogic = loginBusinessLogic;
+            _basketBusinessLogic = basketBusinessLogic;
         }
 
-        public Task<OrderResponse> CreateOrderAsync(Guid basketId)
+        public async Task<OrderResponse> CreateOrderAsync(Guid basketId)
         {
-            throw new NotImplementedException();
+            var basket = await _basketBusinessLogic.GetBasketAsync(basketId);
+            var token = await _loginBusinessLogic.GetTokenAsync();
+            var request = new RestRequest(Endpoints.CREATE_ORDER_ENDPOINT)
+                .AddHeader("Authorization", $"Bearer {token}")
+                .AddJsonBody(basket.OrderRequest);
+            return await _client.PostAsync<OrderResponse>(request);
         }
     }
 }
