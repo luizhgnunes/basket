fa4311905ebb6fe1e7ebdc8c3c5340e439ca8885
Luiz Nunes <lnunes@lohika.com>
2023-06-11 23:42:33 +0100

    Added data access layer

---
+++ Basket.DataAccess/Data/Database.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Common.Models;
+using System.Collections.Concurrent;
+
+namespace Basket.DataAccess.Data
+{
+    internal static class Database
+    {
+        internal static readonly ConcurrentDictionary<Guid, OrderRequest> Basket = new ConcurrentDictionary<Guid, OrderRequest>();
+    }
+}
---
+++ Basket.DataAccess/Repositories/BasketRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace Basket.DataAccess.Repositories
+{
+    internal class BasketRepository
+    {
+        public bool Add(Common.Models.Basket basket)
+        {
+            return true;
+        }
+
+        public IEnumerable<Common.Models.Basket> GetAll()
+        {
+            return null;
+        }
+
+        public bool CheckIfBasketExists(Guid basketId)
+        {
+            return true;
+        }
+
+        public Common.Models.Basket GetBasket(Guid basketId)
+        {
+            return null;
+        }
+
+        public Common.Models.Basket UpdateBasket(Common.Models.Basket basket)
+        {
+            return null;
+        }
+
+        public bool Delete(Guid basketId)
+        {
+            return true;
+        }
+    }
+}
